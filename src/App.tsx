045857b8ddb3e46fdd586aa24cbaa296f7692122
rodrigomsrocha <rodrigomtc82@gmail.com>
2022-07-18 20:59:32 -0300

    Input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import styles from "./App.module.css";
 import { Header } from "./components/Header";
+import { Input } from "./components/Input";
 
 export function App() {
   return (
     <div>
       <Header />
+      <main className={styles.wrapper}>
+        <Input />
+      </main>
     </div>
   );
 }
